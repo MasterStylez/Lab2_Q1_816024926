adbda1c819b2379a01ea2f02e8eea4a7bd54e752
Joshua Elcock <90487177+MasterStylez@users.noreply.github.com>
2022-11-11 23:37:04 -0400

    Delete build directory

--- build/bootloader/bootloader_support/src/flash_qio_mode.d
+++
@@ -1,6 +0,0 @@
-src/flash_qio_mode.o: \
- /opt/esp_sdk/ESP8266_RTOS_SDK/components/bootloader_support/src/flash_qio_mode.c \
- /home/joshuaelcock/esp/Lab2P1/build/bootloader/../include/sdkconfig.h
-
-/home/joshuaelcock/esp/Lab2P1/build/bootloader/../include/sdkconfig.h:
-/opt/esp_sdk/ESP8266_RTOS_SDK/components/bootloader_support/src/flash_qio_mode.c:
--- build/bootloader_support/src/bootloader_random.d
+++
@@ -1,6 +0,0 @@
-src/bootloader_random.o: \
- /opt/esp_sdk/ESP8266_RTOS_SDK/components/bootloader_support/src/bootloader_random.c \
- /home/joshuaelcock/esp/Lab2P1/build/include/sdkconfig.h
-
-/home/joshuaelcock/esp/Lab2P1/build/include/sdkconfig.h:
-/opt/esp_sdk/ESP8266_RTOS_SDK/components/bootloader_support/src/bootloader_random.c:
