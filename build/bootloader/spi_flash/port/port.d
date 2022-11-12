dc79bb719a7032671f4f13e3cf796e1cd40682c4
Joshua Elcock <90487177+MasterStylez@users.noreply.github.com>
2022-11-11 23:29:25 -0400

    Delete build directory

--- build/bootloader/spi_flash/port/port.d
+++
@@ -1,6 +0,0 @@
-port/port.o: \
- /opt/esp_sdk/ESP8266_RTOS_SDK/components/spi_flash/port/port.c \
- /home/joshuaelcock/esp/Lab2P1/build/bootloader/../include/sdkconfig.h
-
-/home/joshuaelcock/esp/Lab2P1/build/bootloader/../include/sdkconfig.h:
-/opt/esp_sdk/ESP8266_RTOS_SDK/components/spi_flash/port/port.c:
